7f34b963f3e30a387e1636a0382385202fd3563a
Emel2099 <126031904+Emel2099@users.noreply.github.com>
2023-05-26 08:39:43 -0500

    Add files via upload

---
+++ Ejercicio 32/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ejercicio_32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pedir N números por pantalla y guardarlos en vector. Mostrar el número máximo y el número mínimo.
+
+            int n, maximo, minimo;
+
+            Console.Write("´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+            Console.Write("Ingresa el tope de números a ingresar ");
+            Console.WriteLine("´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´");
+            
+
+            n = int.Parse(Console.ReadLine());
+
+            int[] numbs = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Ingresar el número {0}: ", i + 1);
+                numbs [i] = int.Parse(Console.ReadLine());
+            }
+
+            maximo = numbs[0];
+            minimo = numbs[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (numbs[i] > maximo)
+                    maximo = numbs[i];
+
+                if (numbs[i] < minimo)
+                    minimo = numbs[i];
+            }
+
+            Console.WriteLine("El número máximo es: " + maximo);
+            Console.WriteLine("El número mínimo es: " + minimo);        
+        }
+    }
+}
